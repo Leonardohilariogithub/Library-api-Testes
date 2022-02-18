c8191823f3d950dc30454629f8ee0f45069c69ec
Leonardo <66699109+Leonardohilariogithub@users.noreply.github.com>
2022-02-18 13:11:29 -0300

    add aula correção com Flavius

---
+++ src/main/java/com/projetorest/libraryapi/services/impl/BookServiceImpl.java
@@ -0,0 +1,21 @@
+package com.projetorest.libraryapi.services.impl;
+
+import com.projetorest.libraryapi.models.Book;
+import com.projetorest.libraryapi.models.repository.BookRepository;
+import com.projetorest.libraryapi.services.BookService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    private BookRepository bookRepository;
+
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+}
